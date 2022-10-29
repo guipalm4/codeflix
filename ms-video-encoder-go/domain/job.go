3602b50ac9634171026dea4d1043076a28f7620c
guipalm4 <gomes.lmc@gmail.com>
2022-10-29 19:51:56 -0300

    Create job repository

--- ms-video-encoder-go/domain/job.go
+++ ms-video-encoder-go/domain/job.go
@@ -11,7 +11,8 @@ type Job struct {
 	ID               string    `valid:"uuid" json:"job_id" gorm:"type:uuid;primary_key"`
 	OutputBucketPath string    `valid:"notnull" json:"output_bucket_path"`
 	Status           string    `valid:"notnull" json:"status"`
-	Video            *Video    `valid:"-" json:"video" gorm:"column:video_id;type:uuid;notnull"`
+	Video            *Video    `valid:"-" json:"video"` 
+	VideoID 				 string 	 `valid:"-" json:"video_id" gorm:"column:video_id;type:uuid;notnull"`
 	Error            string    `valid:"-" json:"error"`
 	CreatedAt        time.Time `valid:"-" json:"created_at"`
 	UpdatedAt        time.Time `valid:"-" json:"updated_at"`
