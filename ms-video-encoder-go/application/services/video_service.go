abed7d315120783f6a76f84c77e29bee93cbac4c
guipalm4 <gomes.lmc@gmail.com>
2022-11-01 22:50:53 -0300

    Add encode method

--- ms-video-encoder-go/application/services/video_service.go
+++ ms-video-encoder-go/application/services/video_service.go
@@ -12,10 +12,12 @@ import (
 	"cloud.google.com/go/storage"
 )
 
+var StoragePath = os.Getenv("LOCAL_STORAGE_PATH")
+var Separator = string(os.PathSeparator)
 
 type VideoService struct {
-	Video	*domain.Video
-	VideoRepository repositories.VideoRepository	
+	Video           *domain.Video
+	VideoRepository repositories.VideoRepository
 }
 
 func NewVideoService() VideoService {
@@ -23,36 +25,36 @@ func NewVideoService() VideoService {
 }
 
 func (v *VideoService) Download(bucketName string) error {
-	
-	ctx := context.Background()	
+
+	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 
-	if err!= nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
 	bucket := client.Bucket(bucketName)
 	obj := bucket.Object(v.Video.FilePath)
 	reader, err := obj.NewReader(ctx)
 
-	if err!= nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 	defer reader.Close()
 	body, err := ioutil.ReadAll(reader)
-	if err!= nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-	file, err := os.Create(os.Getenv("LOCAL_STORAGE_PATH") + string(os.PathSeparator) + v.Video.ID + ".mp4")
-	if err!= nil {
-    return err
-  }
+	file, err := os.Create(getVideoPath(v) + ".mp4")
+	if err != nil {
+		return err
+	}
 
 	_, err = file.Write(body)
-	if err!= nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 	defer file.Close()
 
 	log.Printf("video %v has been stored", v.Video.ID)
@@ -61,27 +63,47 @@ func (v *VideoService) Download(bucketName string) error {
 
 func (v *VideoService) Fragment() error {
 
-	path := os.Getenv("LOCAL_STORAGE_PATH")
-	separator := string(os.PathSeparator)
-	videoID := v.Video.ID
-	
-	err := os.MkdirAll(path + separator + videoID, os.ModePerm)
-  if err!= nil {
-    return err
-  }
-	
-	source := path + separator + videoID + ".mp4"
-	target := path + separator + videoID + ".frag"
+	err := os.MkdirAll(getVideoPath(v), os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	source := getVideoPath(v) + ".mp4"
+	target := getVideoPath(v) + ".frag"
 
 	cmd := exec.Command("mp4fragment", source, target)
 	output, err := cmd.CombinedOutput()
 
-  if err!= nil {
+	if err != nil {
 		return err
 	}
-	
+
 	printOutput(output)
 	return nil
+}
+
+func (v *VideoService) Encode() error {
+
+	cmdArgs := []string{}
+	cmdArgs = append(cmdArgs, getVideoPath(v)+".frag")
+	cmdArgs = append(cmdArgs, "--use-segment-timeline")
+	cmdArgs = append(cmdArgs, "-o")
+	cmdArgs = append(cmdArgs, getVideoPath(v))
+	cmdArgs = append(cmdArgs, "-f")
+	cmdArgs = append(cmdArgs, "--exec-dir")
+	cmdArgs = append(cmdArgs, "/opt/bento4/bin/")
+	cmd := exec.Command("mp4dash", cmdArgs...)
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return err
+	}
+
+	printOutput(output)
+
+	return nil
+
 }
 
 func printOutput(out []byte) {
@@ -90,3 +112,6 @@ func printOutput(out []byte) {
 	}
 }
 
+func getVideoPath(v *VideoService) string {
+	return StoragePath + Separator + v.Video.ID
+}
